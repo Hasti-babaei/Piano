14d2ab91d263b03db5e758b8753fa9abf9ce902e
Hasti-babaei <beingbabaei@gmail.com>
2023-10-11 14:32:47 +0330

    codes cleaning

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-const note = document.querySelector(".nowplaying");
+const note = document.querySelector(".nowPlaying");
 const keys = document.querySelectorAll(".key");
 const hints = document.querySelectorAll(".hints");
 
 window.addEventListener("keydown", function (e) {
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-
   if (!key) return;
-
   const keyNote = key.getAttribute("data-note");
-
   key.classList.add("playing");
   note.innerHTML = keyNote;
   audio.currentTime = 0;
@@ -19,6 +16,7 @@ window.addEventListener("keydown", function (e) {
 function removeTransition() {
   this.classList.remove("playing");
 }
+
 hints.forEach(function (elm, index) {
   elm.style = `transition-delay : ${index * 50}ms`;
 });
